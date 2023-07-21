c0d67adb46094441f7c4014281f6e2173cdfe40e
jineshdas <jineshjd2@gmail.com>
2023-07-21 20:44:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeLanguage(event){
+    var lang=event.value
+    if (lang=="Hindi") {
+        window.open("hindi.html","_self")
+    }
+    else{
+        window.open("index.html","_self")
+    }
+}
